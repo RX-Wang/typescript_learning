9d473c73d11bda5028103450c2236ca39cc853b2
wangxuquan <wangxuquan@meituan.com>
2018-07-16 15:34:14 +0800

    添加：数组类型的学习

--- src/ts1.ts
+++ src/ts1.ts
@@ -2,12 +2,43 @@ let aBoolean: boolean = true;
 let aNumber: number = 12;
 let aString: string = 'String';
 
-// 数组
+/** 1、数组 */
 let aArray: number[] = [1, 2, 3];
 let anotherArray: Array<number> = [1, 2, 3];
 
-// 元组
-let x: [number, string] = [0, '1']; // 数组元素必须跟元组 类型一一对应。
+/** 2、元组 */
+let x: [number, string] = [0, '1']; // 数组元素必须跟元组 类型,及长度 一一对应。
 x[2] = '2';
 
-console.log(x);
+// console.log(x);
+
+/** 3、枚举，元素默认从0 开始。 */
+enum Color {Red = 1, Green, Blue};
+let colorName: string = Color[2];
+// console.log(Color);
+// console.log(colorName);
+
+/** 4、any  让变量不进行类型检查，直接通过编译 */
+
+let aAny: any = 'any';
+aAny = 4;
+aAny = false;
+
+// 数组类型 any
+let anyAray: any[] = [true, 1, '2', [1, 2, 3, 4], {name: 'Diana'}];
+console.log(anyAray);
+
+// 泛型 any
+let aAnyArray: Array<any> = [1, '2', true, {name: 'Diana'}];
+console.log(aAnyArray);
+
+// 元组 any
+let anotherAnyArray: [any] = [true];
+anotherAnyArray[1] = 1;
+console.log(anotherAnyArray);
+
+/** 5、Void  没有任何类型,或函数没有任何返回值 */
+
+function aVoidFunc(): void {
+  console.log('this is a void function');
+}
